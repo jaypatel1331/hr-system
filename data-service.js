2c5a415882e6c7f2dacb101fd6b042464c7fc3aa
Jay Patel <Jaykumarpatel2410@gmail.com>
2021-09-24 20:37:14 -0400

    Initial commit

---
+++ data-service.js
@@ -0,0 +1,83 @@
+var fs = require('fs');
+
+var employees = [];
+var departments = [];
+
+
+module.exports = {
+
+    initialize: function() {
+        let promise = new Promise(function(resolve, reject) {
+            
+            fs.readFile('./data/employees.json','utf8',(err,data) => {
+                if (err) {
+                    reject("Error! employees.json could not be loaded!");
+                } else {
+
+                    employees = JSON.parse(data);
+                    console.log("Succuss! employees.json loaded!");
+
+                    fs.readFile('./data/departments.json','utf8',(err,data) => {
+                        if (err) {
+                            reject("Error! departments.json could not be loaded!");
+                        } else {
+
+                            departments = JSON.parse(data);
+                            console.log("Succuss! departments.json loaded!");
+                            resolve('Server initialization successful!');
+                        }
+                    });
+                }
+            });
+
+
+
+        });
+
+        return promise;
+    },
+
+    getEmployees: function() {
+        let promise = new Promise(function(resolve,reject) {
+            if (employees.length > 0) {
+                resolve(employees);
+            } else {
+                reject('No results returned!');
+            }
+        });
+        
+        return promise;
+    },
+    
+    getManagers: function() {
+        let managers = []; 
+        let promise = new Promise(function(resolve, reject) {
+
+            for (let i = 0; i < employees.length; i++) {
+
+                if (employees[i].isManager == true)
+                    managers.push(employees[i]);
+
+                if (managers.length > 0) {
+                    resolve(managers);
+                } else {
+                    reject("No results returned!");
+                }
+            }
+        });
+
+        return promise;
+    },
+    
+    getDepartments: function() {
+        let promise = new Promise(function(resolve,reject) {
+            if (departments.length > 0) {
+                resolve(departments);
+            } else {
+                reject("No results returned");
+            }
+        });
+
+        return promise;
+    },
+};
--- server.js
+++ server.js
@@ -1,28 +1,66 @@
+var data = require('./data-service.js')
 var express = require("express");
 var app = express();
-
 var path = require("path");
-
 var HTTP_PORT = process.env.PORT || 8080;
 
-// function wil be called after the start of http server for listening for requests
-function onHttpStart(){
-    console.log("Express http server listening on port" + HTTP_PORT);
-}
+app.use(express.static('public'));
 
-// setup root to listen on the default url part 
+
 app.get("/", function(request, response){
     response.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
-// set up another route for the about page
-app.get("/about", function(request, response){
-    // response.send("<h3> About </h3>");
 
+app.get("/about", function(request, response){
     response.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
-//set up http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
 
-// use ctrl +c to stop server
+app.get("/employees", function(request,response) {
+    data.getEmployees()
+    .then(function(value) {
+        response.json(value);
+    })
+    .catch(function(error) {
+        response.json({message: error});
+    });
+    
+});
+
+
+app.get("/managers", function(request,response) {
+    data.getManagers()
+    .then(function(value) {
+        response.json(value);
+    })
+    .catch(function(error) {
+        response.json({message: error});
+    });
+});
+
+
+app.get("/departments", function(request,response) {
+    data.getDepartments()
+    .then(function(value) {
+        response.json(value);
+    })
+    .catch(function(error) {
+        response.json({message: error});
+    });
+});
+
+
+app.use(function(request,response,next) {
+    response.status(404).send('Page not found, yo. 404');
+});
+
+
+data.initialize()
+.then(function(message) {
+    console.log(message);
+    app.listen(HTTP_PORT);
+})
+.catch(function(error) {
+    console.log(error);
+});

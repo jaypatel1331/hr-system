f43565cfca2f1ff8f8317a04b9ab94226020e24b
Jay Patel <jaykumarpatel2410@gmail.com>
2023-08-13 15:52:34 -0400

    first commit

--- server.js
+++ server.js
@@ -1,283 +1,314 @@
-/*********************************************************************************
-*  WEB322 – Assignment 06
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
-*  (including 3rd party web sites) or distributed to other students.
-*
-*  Name: Jay Ashishbhai Patel
-*  Student ID: 154925192
-*  Date: 2 December 2021
-*
-*  Online (Heroku) Link: https://stark-harbor-91923.herokuapp.com/ 
-*
-********************************************************************************/
-
-const data = require('./data-service.js');
-const dataServiceAuth = require('./data-service-auth.js');
-
-const clientSessions = require("client-sessions");
 const express = require("express");
-const multer = require("multer");
 const bodyParser = require("body-parser");
-const fs = require("fs");
-const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+const oracledb = require("oracledb");
+const dbConfig = {
+  user: "dbs501_232v1a14",
+  password: "29202799",
+  connectString:
+    "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = myoracle12c.senecacollege.ca)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = oracle12c)))",
+};
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const storage = multer.diskStorage({
-    destination: "./public/images/uploaded",
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-
-
-app.engine('.hbs', exphbs({
-    extname: '.hbs',
-    defaultLayout: 'main',
+app.engine(
+  ".hbs",
+  exphbs({
+    extname: ".hbs",
+    defaultLayout: "main",
     helpers: {
-        navLink: function (url, options) {
-            return '<li' +
-                ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
-                '><a href="' + url + '">' + options.fn(this) + '</a></li>';
-        },
-        equal: function (lvalue, rvalue, options) {
-            if (arguments.length < 3)
-                throw new Error("Handlebars Helper equal needs 2 parameters");
-            if (lvalue != rvalue) {
-                return options.inverse(this);
-            } else {
-                return options.fn(this);
-            }
+      navLink: function (url, options) {
+        return (
+          "<li" +
+          (url == app.locals.activeRoute ? ' class="active" ' : "") +
+          '><a href="' +
+          url +
+          '">' +
+          options.fn(this) +
+          "</a></li>"
+        );
+      },
+      equal: function (lvalue, rvalue, options) {
+        if (arguments.length < 3)
+          throw new Error("Handlebars Helper equal needs 2 parameters");
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
         }
-    }
-}));
+      },
+    },
+  })
+);
 
-app.set('view engine', '.hbs');
+app.set("view engine", ".hbs");
 
 app.use(function (req, res, next) {
-    let route = req.baseUrl + req.path;
-    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
-    next();
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = route == "/" ? "/" : route.replace(/\/$/, "");
+  next();
 });
-
-const upload = multer({ storage: storage });
-
-app.use(clientSessions({
-    cookieName: "session",
-    secret: "Web_assignment_6",
-    duration: 2 * 60 * 1000,
-    activeDuration: 1000 * 60 
-}));
-
-app.use(function(req, res, next) {
-    res.locals.session = req.session;
-    next();
-   });
-
-   function ensureLogin(req, res, next) {
-    if (!req.session.user) {
-        res.redirect("/login");
-    } else {
-        next();
-    }
-}
 
 app.get("/", function (req, res) {
-    res.render('home');
-});
-
-app.get("/about", function (req, res) {
-    res.render('about');
-});
-
-app.get("/employees",ensureLogin, function (req, res) {
-    if (req.query.status) {
-        data.getEmployeesByStatus(req.query.status)
-            .then((value) => res.render('employees', { employees: value }))
-            .catch(err => res.status(404).send('no results'));
-
-    } else if (req.query.department) {
-        data.getEmployeesByDepartment(req.query.department)
-            .then((value) => res.render('employees', { employees: value }))
-            .catch(err => res.status(404).send('no results'));
-
-    } else if (req.query.manager) {
-        data.getEmployeesByManager(req.query.manager)
-            .then((value) => res.render('employees', { employees: value }))
-            .catch(err => res.status(404).send('no results'));
-    }
-    else {
-        data.getAllEmployees()
-            .then(value => res.render("employees", { employees: value }))
-            .catch(err => res.status(404).send('no results'));
-    }
-});
-
-app.get("/employees/add", ensureLogin, function (req, res) {
-    data.getDepartments()
-        .then(data => res.render("addEmployee", { departments: data }))
-        .catch(err => res.render("addEmployee", { departments: [] }));
+  res.render("home");
 });
 
 
-app.post("/employees/add", ensureLogin, function (req, res) {
-    data.addEmployee(req.body)
-        .then(res.redirect('/employees'));
-});
-
-app.get("/employee/:empNum", ensureLogin, (req, res) => {
-    // initialize an empty object to store the values
-    let viewData = {};
-    data.getEmployeeByNum(req.params.empNum).then((data) => {
-        if (data) {
-            viewData.employee = data; //store employee data in the "viewData" object as "employee"
-        } else {
-            viewData.employee = null; // set employee to null if none were returned
-        }
-    }).catch(() => {
-        viewData.employee = null; // set employee to null if there was an error
-    }).then(data.getDepartments)
-        .then((data) => {
-            viewData.departments = data; // store department data in the "viewData" object as "departments"
-            // loop through viewData.departments and once we have found the departmentId that matches
-            // the employee's "department" value, add a "selected" property to the matching
-            // viewData.departments object
-            for (let i = 0; i < viewData.departments.length; i++) {
-                if (viewData.departments[i].departmentId == viewData.employee.department) {
-                    viewData.departments[i].selected = true;
-                }
-            }
-        }).catch(() => {
-            viewData.departments = []; // set departments to empty if there was an error
-        }).then(() => {
-            if (viewData.employee == null) { // if no employee - return an error
-                res.status(404).send("Employee Not Found");
-            } else {
-                res.render("employee", { viewData: viewData }); // render the "employee" view
-            }
-        });
-});
-
-app.post("/employee/update", ensureLogin, (req, res) => {
-    data.updateEmployee(req.body).then(res.redirect("/employees"));
-});
-
-app.get('/employees/delete/:value', ensureLogin, (req, res) => {
-    data.deleteEmployeeByNum(req.params.value)
-        .then(res.redirect("/employees"))
-        .catch(err => res.status(500).send("Unable to Remove Employee / Employee not found"));
-});
-
-app.get("/departments", ensureLogin, function (req, res) {
-    data.getDepartments()
-        .then(function (value) {
-            res.render('departments', { departments: value });
-        })
-        .catch(err => res.status(404).send('no results'));
-});
-
-app.get("/departments/add", ensureLogin, (req, res) => {
-    res.render(path.join(__dirname + "/views/addDepartment.hbs"));
-});
-
-app.post("/departments/add", ensureLogin, (req, res) => {
-    data.addDepartment(req.body).then(() => {
-        res.redirect("/departments");
+app.get("/employees", (req, res) => {
+  const connection = req.app.locals.connection;
+  connection
+    .execute("SELECT * FROM hr_employees")
+    .then((result) => {
+      res.render("employees", { employees: result.rows });
     })
-});
-
-app.post("/department/update", ensureLogin, (req, res) => {
-    data.updateDepartment(req.body).then(() => {
-        res.redirect("/departments");
-    })
-});
-
-app.get("/department/:departmentId", ensureLogin, (req, res) => {
-    data.getDepartmentById(req.params.departmentId)
-        .then((data) => { res.render("department", { department: data }) })
-        .catch(err => res.status(404).send("department not found"))
-});
-
-app.get('/departments/delete/:departmentId', ensureLogin, (req, res) => {
-    data.deleteDepartmentById(req.params.departmentId)
-        .then(res.redirect("/departments"))
-        .catch(err => res.status(500).send("Unable to Remove Department / Department not found"))
-});
-
-app.get("/images/add", ensureLogin, function (req, res) {
-    res.render('addImage');
-});
-
-app.post("/images/add", ensureLogin, upload.single("imageFile"), function (req, res) {
-    res.redirect('/images');
-});
-
-app.get("/images", ensureLogin, function (req, res) {
-    fs.readdir(path.join(__dirname, "/public/images/uploaded"),
-        function (err, items) {
-            res.render('images', { images: items });
-        });
-});
-
-app.get("/login", function(req, res) {
-    res.render('login');
-});
-
-app.get("/register", function(req, res) { 
-    res.render('register');
-});
-
-app.post("/register", function(req, res) {
-    dataServiceAuth.registerUser(req.body)
-    .then(() => res.render('register', { successMessage: "User created!"}))
-    .catch((err) => res.render('register', { errorMessage: err, userName: req.body.userName }));
-});
-
-app.post("/login", function(req, res) {
-    req.body.userAgent = req.get('User-Agent');
-
-    dataServiceAuth.checkUser(req.body)
-    .then(function(user) { 
-        req.session.user = {
-            userName: user.userName,
-            email: user.email,
-            loginHistory: user.loginHistory
-        }
-
-        res.redirect("/employees");
-    })
-    .catch(function(err) {
-        console.log(err);
-        res.render('login', { errorMessage: err, userName: req.body.userName });
+    .catch((err) => {
+      console.log("Error fetching employees from the database:", err);
+      res.status(500).json({ error: "error from database" });
     });
 });
 
-app.get("/logout", function(req, res) {
-    req.session.reset();
-    res.redirect('/');
+app.get("/employees/add", function (req, res) {
+  const connection = req.app.locals.connection;
+  let data = {};
+
+  data.date = new Date();
+  
+    connection
+        .execute("SELECT * FROM hr_departments")
+        .then((result1) => {
+            data.departments = result1.rows;
+            connection
+                .execute("SELECT * FROM hr_employees where employee_id in (select manager_id from hr_employees)")
+                .then((result2) => {
+                    data.managers = result2.rows;
+                    connection
+                        .execute("SELECT * FROM hr_jobs")
+                        .then((result3) => {
+                            data.jobs = result3.rows;
+                            res.render("addEmployee", { data: data });
+                        })
+                        .catch((err) => {
+                          console.log("Error fetching jobs from the database:", err);
+                          res.status(500).json({ error: "error from database" });
+                        });
+                })
+                .catch((err) => {
+                  console.log("Error fetching managers from the database:", err);
+                  res.status(500).json({ error: "error from database" });
+                }
+            );
+        })
+        .catch((err) => {
+          console.log("Error fetching departments from the database:", err);
+          res.status(500).json({ error: "error from database" });
+        }
+    );
 });
 
-app.get("/userHistory", ensureLogin, function (req, res) {
-    res.render('userHistory');
-}); 
+
+// Assuming you have already defined 'app' and set up other required middleware
+
+app.post("/employees/add", function (req, res) {
+  const connection = req.app.locals.connection;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const hireDate = req.body.hiredate;
+  const jobId = req.body.jobId;
+  const salary = req.body.salary;
+  const managerId = req.body.managerId;
+  const departmentId = req.body.departmentId;
+
+    const formattedHireDate = new Date(hireDate).toISOString().slice(0, 10);
+  connection
+    .execute(
+      `BEGIN hr_employee_hire_sp(:firstName, :lastName, :email, :salary, TO_DATE(:hireDate, 'YYYY-MM-DD'), :phone, :jobId, :managerId, :departmentId); END;`,
+      {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        salary: salary,
+        hireDate: formattedHireDate,
+        phone: phone,
+        jobId: jobId,
+        managerId: managerId,
+        departmentId: departmentId,
+      },
+      { autoCommit: true }
+    )
+    .then((result) => {
+      console.log("Employee added successfully.");
+      res.redirect("/employees"); // Redirect to employees page
+    })
+    .catch((err) => {
+      console.error("Error inserting into the database:", err);
+      res.status(500).send("Error adding employee.");
+    });
+});
+
+
+
+app.get("/employees/:empnum", (req, res) => {
+  const connection = req.app.locals.connection;
+  connection
+    .execute("SELECT * FROM hr_employees WHERE employee_id = :empNum", [
+      req.params.empnum,
+    ])
+    .then((result) => {
+      res.render("employee", { employees: result.rows });
+    })
+    .catch((err) => {
+      console.log("Error fetching data from the database:", err);
+      res.status(500).json({ error: "error from database" });
+    });
+});
+
+// post the update data to the oracledb and redirect to the employees page
+app.post("/employee/update", function (req, res) {
+  const connection = req.app.locals.connection;
+  const empNum = req.body.employee_id;
+  const salary = req.body.salary;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  connection
+    .execute(
+      `UPDATE hr_employees SET salary= :salary, phone_number= :phone, email= :email WHERE employee_id= :empNum`,
+      {
+        salary,
+        phone,
+        email,
+        empNum
+      },
+      { autoCommit: true }
+  )
+    .then((result) => {
+      res.redirect("/employees");
+    })
+    .catch((err) => {
+      console.log("Error updating employee from the database:", err);
+      res.status(500).json({ error: "error from database" });
+    });
+});
+
+
+app.get("/jobs", (req, res) => {
+  const connection = req.app.locals.connection;
+  connection
+    .execute("SELECT * FROM hr_jobs")
+    .then((result) => {
+      res.render("jobs", { jobs: result.rows });
+    })
+    .catch((err) => {
+      console.log("Error fetching jobs from the database:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+app.get("/jobs/add", function (req, res) {
+  res.render("addJob");
+});
+
+
+app.post("/jobs/add", function (req, res) {
+  const connection = req.app.locals.connection;
+  const jobId = req.body.jobID;
+  const jobTitle = req.body.jobTitle;
+  const minSalary = req.body.min_salary;
+  connection
+    .execute(
+      `BEGIN NEW_JOB(:jobId, :jobTitle, :minSalary); END;`,
+      {
+        jobId: jobId,
+        jobTitle: jobTitle,
+        minSalary: minSalary,
+      },
+      { autoCommit: true }
+  )
+    .then((result) => {
+      res.redirect("/jobs");
+    })
+    .catch((err) => {
+      console.log("Error inserting job to the database:", err);
+      res.status(500).json({ error: "error from database" });
+    });
+});
+
+// get the job id and render the job view with oracledb
+app.get("/jobs/:jobId", (req, res) => {
+  const connection = req.app.locals.connection;
+  connection
+    .execute("SELECT * FROM hr_jobs WHERE job_id = :jobId", [
+      req.params.jobId,
+    ])
+    .then((result) => {
+      res.render("job", { jobs: result.rows });
+    })
+    .catch((err) => {
+      console.log("Error fetching job from the database:", err);
+      res.status(500).json({ error: "error from database" });
+    });
+});
+
+
+// post the update data to the oracledb and redirect to the jobs page
+app.post("/job/update", function (req, res) {
+  const connection = req.app.locals.connection;
+  const jobId = req.body.jobId;
+  const jobTitle = req.body.jobTitle;
+  const minSalary = req.body.min_salary;
+  const maxSalary = req.body.max_salary;
+  connection
+    .execute(
+      `UPDATE hr_jobs SET job_title= :jobTitle, min_salary= :minSalary, max_salary= :maxSalary WHERE job_id= :jobId`,
+      {
+        jobTitle,
+        minSalary,
+        maxSalary,
+        jobId,
+      },
+      { autoCommit: true }
+  )
+    .then((result) => {
+      res.redirect("/jobs");
+    })
+    .catch((err) => {
+      console.log("Error updating job from the database:", err);
+      res.status(500).json({ error: "error from database" });
+    });
+});
+
+app.get("/departments", function (req, res) {
+  const connection = req.app.locals.connection;
+  connection
+      .execute("SELECT * FROM hr_departments")
+      .then((result) => {
+          res.render("departments", { departments: result.rows });
+      })
+      .catch((err) => {
+          console.log("Error fetching departments from the database:", err);
+          res.status(500).json({ error: "Internal Server Error" });
+      });
+});
 
 app.use(function (req, res, next) {
-    res.status(404).send('Page not found, Error: 404');
+  res.status(404).send("Page not found, Error: 404");
 });
 
-data.initialize()
-.then(dataServiceAuth.initialize)
-    .then(function (message) {
-        console.log(message);
-        app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, () => {
+  oracledb
+    .getConnection(dbConfig)
+    .then((connection) => {
+      console.log("Connected to the Oracle database!");
+      app.locals.connection = connection;
     })
-    .catch(function (err) {
-        console.log(err);
+    .catch((err) => {
+      console.log("Error connecting to the Oracle database:", err);
     });
+});
